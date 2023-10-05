179f3b74791c268cb70c29c96847b3ba304b727b
NguyenAnhCuong <cuong1606x@gmail.com>
2023-10-05 13:40:49 +0700

    add api

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,19 @@
-
+import axiosClient from "./api/axiosClient";
+import axios from "axios";
+import postApi from "./api/postApi";
+async function main(){
+    // const response = await axiosClient.get('/posts')
+    // console.log(response);
+    const queryParams ={
+        _page:1,
+        _limit:3,
+    }
+    const response = await postApi.getAll(queryParams)
+    console.log(response);
+    postApi.add({
+        title:'',
+        author:'',
+        
+    })
+}
+main()
