3a76a3ebe93771984dd3a9a35912f60df65316b1
NguyenAnhCuong <cuong1606x@gmail.com>
2023-10-11 14:51:19 +0700

    search event and pre-next links completed

--- JS/home.js
+++ JS/home.js
@@ -1,10 +1,12 @@
 import axiosClient from "./api/axiosClient";
 import axios from "axios";
 import postApi from "./api/postApi";
-import { setTextContent, truncatText } from "./util";
+import { getPagination, setTextContent, truncatText } from "./util";
 import dayjs from "dayjs";
+import debounce from "lodash.debounce";
 import relativeTime from 'dayjs/plugin/relativeTime' 
 dayjs.extend(relativeTime)
+
 
 async function main(){
     // const response = await axiosClient.get('/posts')
@@ -60,7 +62,7 @@ function createPostElement(post){
     if (thumbnailElement) {
         thumbnailElement.src = post.imageUrl
         thumbnailElement.addEventListener('error',()=>{
-            thumbnailElement.src = 'https://via.placeholder.com'
+            thumbnailElement.src = ''
         })
     }
     
@@ -75,34 +77,77 @@ function createPostElement(post){
 
 function renderPostList(postList){
     console.log({postList});
-    if (!Array.isArray(postList) || postList.length === 0 ) return;
+    if (!Array.isArray(postList) ) return;
     const ulElement = document.getElementById('postList')
     if (!ulElement) return
+    //clear current list
+    ulElement.textContent = ''
+
     postList.forEach((post) =>{
         const liElement = createPostElement(post)
         ulElement.appendChild(liElement)
     })
 }
-function handleFilterChange(filterName,filterValue){
-    
-    const url = new URL(window.location)
-    url.searchParams.set(filterName,filterValue)
-    history.pushState({},'',url)
+function renderPagination(pagination){
+    const ulPagination = document.getElementById('pagination')
+    //calc totalPages
+    if (!pagination | !ulPagination) return
+    const {_page,_limit,_totalRows} = pagination
+    const totalPages = Math.ceil(_totalRows / _limit)
+    //save page and totalPage to ulPagination
+    ulPagination.dataset.page = _page
+    ulPagination.dataset.totalPages = totalPages
 
+    //check if enable/disable pre/next linkd
+    if (_page <= 1) ulPagination.firstElementChild?.classList.add('disabled')
+    else ulPagination.firstElementChild?.classList.remove('disabled')
+
+    if (_page >= totalPages) ulPagination.lastElementChild?.classList.add('disabled')
+    else ulPagination.lastElementChild?.classList.remove('disabled')
+}
+async function handleFilterChange(filterName,filterValue){
+    
+    try {
+        const url = new URL(window.location)
+        url.searchParams.set(filterName,filterValue)
+        if (filterName === 'title_like') url.searchParams.set('_page',1)
+        history.pushState({},'',url)
+
+        //fetch API
+        //re render post list
+        const {data,pagination} = await postApi.getAll(url.searchParams)
+        renderPostList(data)
+        renderPagination(pagination)
+    } catch (error) {
+        console.log('fail to catch post list',error);
+    }
 }
 
 function handlePreClick(e){
     e.preventDefault()
+    console.log('pre click');
+    const ulPagination = getPagination()
+    if (!ulPagination) return
+    const page = Number.parseInt(ulPagination.dataset.page) || 1
+    if (page <= 1) return
 
+    handleFilterChange('_page',page - 1)
 }
 function handleNextClick(e){
     e.preventDefault()
+    console.log('next click');
+    const ulPagination = getPagination()
+    if (!ulPagination) return
+    const page = Number.parseInt(ulPagination.dataset.page) || 1
+    const totalPages = ulPagination.dataset.totalPages
+    if (page >= totalPages) return
 
+    handleFilterChange('_page',page + 1)
 }
 
 function InitPagination(){
     //bind click for pre - next link
-    const ulPagination = document.getElementById('pagination')
+    const ulPagination = getPagination()
     if (!ulPagination) return
     const preLink = ulPagination.firstElementChild?.firstElementChild
     if (preLink){
@@ -125,18 +170,33 @@ function initURL(){
     history.pushState({},'',url)
 
 }
+function initSearch(){
+    const searchInput = document.getElementById('searchInput')
+    if (!searchInput) return
+    const queryParams = new URLSearchParams(window.location.search)
+    if (queryParams.get('title_like')){
+        searchInput.value = queryParams.get('title_like')
+    }
+    const debounceSearch = debounce((event)=>handleFilterChange('title_like',event.target.value),1000)
+    //set default values form query params
+    searchInput.addEventListener('input',debounceSearch)
+}
 
 (async () =>{
     try {
+        //attach event for link
         InitPagination()
+        initSearch()
+        //set default pagination (_page,_limit) on URL
         initURL();
-
+        //render post list based URL params
         const queryParams = new URLSearchParams(window.location.search)
         //set default query params if not existed
         console.log(queryParams.toString());
 
         const {data,pagination} = await postApi.getAll(queryParams)
         renderPostList(data)
+        renderPagination(pagination)
     } catch (error) {
         console.log('get all failed:',error);
     }
